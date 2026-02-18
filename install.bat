73842a645270fd4318d318d6993d2273fcaa71c7
Fabio Matricardi <fabio.matricardi@gmail.com>
2026-02-18 23:16:49 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,18 @@
+@echo off
+echo Downloading the wget binaries...
+curl -L -o wget.exe https://github.com/fabiomatricardi/KSAPPs/raw/main/wget.exe
+echo Downloading the python app folders Archive...
+wget.exe https://github.com/fabiomatricardi/KSAPPs/raw/main/KS_OandM_APPS.zip -nv --show-progress
+echo Downloading python requirements
+wget.exe https://github.com/fabiomatricardi/KSAPPs/raw/main/requirements.txt -nv --show-progress
+echo Unzipping the python app folders...
+tar -xf KS_OandM_APPS.zip
+echo Creating Virtual environment
+python -m venv venv
+echo Activating venv
+call .\venv\Scripts\activate.bat
+echo Installing dependencies
+pip install -r requirements.txt
+echo starting the app...
+timeout /t 10
+start cmd.exe /c runSplash.bat
